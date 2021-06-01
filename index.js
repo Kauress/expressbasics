956afa13e306de2f63b5a479d153457c8285aa6e
kauress <kauress8@gmail.com>
2021-05-31 20:35:16 -0700

    made router dir

--- index.js
+++ index.js
@@ -55,6 +55,9 @@ const PORT = 4040;
 
 const path = require('path');
 
+//new 
+const routes = require("./routes/app")
+
 // View Engine
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
@@ -63,24 +66,9 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-// Get Routing
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-app.get("/contact", (req, res) => {
-    // Please bring me out to dinner first 🍝
-    //res.send("Buy me dinner first");
-    res.render("contact");
-});
+//new
+app.use("/", routes)
 
-// Post Routing
-app.post("/contact", (req, res) => {
-    if(req.body){
-        console.log(req.body.username);
-        console.log(req.body.email);
-        res.render("dashboard",{username: req.body.username, email: req.body.email});
-    }
-});
 
 //Please follow convention
 app.listen(PORT, ()=>{
---
+++ routes/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+router.get("/", function(req, res){
+ res.render("index")
+})
+
+/*
+// Get Routing
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.get("/contact", (req, res) => {
+    // Please bring me out to dinner first 🍝
+    //res.send("Buy me dinner first");
+    res.render("contact");
+});
+
+// Post Routing
+app.post("/contact", (req, res) => {
+    if(req.body){
+        console.log(req.body.username);
+        console.log(req.body.email);
+        res.render("dashboard",{username: req.body.username, email: req.body.email});
+    }
+});
+*/
+
+
+module.exports = router;
